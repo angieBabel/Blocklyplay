61e2b42312b3c54160b247083dc9665be541bb52
Angelica Barreda B <anjelyk.b@gmail.com>
2016-04-20 12:49:16 -0500

    update

---
+++ resources/codeBrico/blockly_helpers.js
@@ -0,0 +1,25 @@
+.progressBarclass {
+    width: 130px;
+    height: 22px;
+    border: 1px solid #111;
+    border-radius: 10px;
+    background: #e6e5e2;
+    box-shadow: 0 -1px 1px #c0bfbc inset;
+}
+
+.progressBarclass div {
+    float:left !important;
+    height: 100%;
+    color: #fff;
+    font-weight: bold;
+    text-align: right;
+    line-height: 22px; /* same as #progressBar height if we want text middle aligned */
+    width: 0px;
+    border-radius: 10px;
+    background-color: #0495C5;
+    background: -webkit-linear-gradient(top, #0495C5, #3DA8D3);
+    background: -webkit-gradient(linear, 0% 0%, 0% 100%, from(#0495C5), to(#3DA8D3));
+    background: -moz-linear-gradient(top, #0495C5, #3DA8D3);
+    background: -ms-linear-gradient(top, #0495C5, #3DA8D3);
+    background: -o-linear-gradient(top, #0495C5, #3DA8D3);
+}
