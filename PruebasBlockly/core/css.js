715f450cb5db300f27f3c4f6ec1c9ff546fed4de
Jordi Alejandro García Núñez <jordign.mx@hotmail.com>
2016-08-25 14:13:21 -0500

    update
    se actualizaron estilos para el color de fondo fuera gris

--- PruebasBlockly/core/css.js
+++ PruebasBlockly/core/css.js
@@ -133,7 +133,8 @@ Blockly.Css.setCursor = function(cursor) {
 Blockly.Css.CONTENT = [
   '.blocklySvg {',
   //'background: linear-gradient(#EDE7F6, #fff);',
-  'background-color: #FAFAFA;', //color por defecto
+  //'background-color: #FAFAFA;', //color por defecto
+  'background-color: #F1F1F1;', //color por defecto
     //'background-color: #EDE7F6;',//aqui se cambia el background del workspace backchange
     'outline: none;',
     'overflow: hidden;',  /* IE overflows by default. */
@@ -400,7 +401,8 @@ Blockly.Css.CONTENT = [
   /* Category tree in Toolbox. */
   '.blocklyToolboxDiv {',
   //'background: linear-gradient(#EDE7F6, #fff); ',
-  'background-color: #FAFAFA;', //color por defecto
+  //'background-color: #FAFAFA;', //color por defecto
+  'background-color: #F1F1F1;', //color por defecto
     //'background-color: #EDE7F6;',// aqui se edita el background color del toolbox backchange
     /*'border-right:  1px solid #E8D7D1;',*/
     'overflow-x: visible;',
